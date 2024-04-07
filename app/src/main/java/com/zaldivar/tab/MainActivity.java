70c4bb5e39e99754a3cb3c202ad11bc8897b5822
RoizkieHere <roiabrera14@gmail.com>
2024-04-07 23:13:15 +0800

    Initial commit

---
+++ app/src/main/java/com/zaldivar/tab/MainActivity.java
@@ -0,0 +1,43 @@
+package com.zaldivar.tab;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager =  findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPadapter vpAdapter = new VPadapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        vpAdapter.addFragment(new pending(), "Pending");
+        vpAdapter.addFragment(new transit(), "On Delivery");
+        vpAdapter.addFragment(new completed(), "Completed");
+
+
+        viewPager.setAdapter(vpAdapter);
+
+
+
+    }
+}
