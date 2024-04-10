8a9d0d11cf07deebd9e894e98b36fba5ab62d9db
RoizkieHere <roiabrera14@gmail.com>
2024-04-10 21:08:49 +0800

    6:46 PM

--- app/src/main/java/com/zaldivar/tab/Pending.java
+++ app/src/main/java/com/zaldivar/tab/Pending.java
@@ -3,11 +3,16 @@ package com.zaldivar.tab;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
@@ -21,66 +26,89 @@ import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Pending extends Fragment {
 
-    Context context;
-    String url = "https://zaldivarservices.com/customer_app/pending.php";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        context = getContext();
+
+        View rootview = inflater.inflate(R.layout.fragment_pending, container, false);
+
+        String url = "https://zaldivarservices.com/customer_app/pending.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(rootview.getContext());
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Toast.makeText(getContext(), "qwwefwef", Toast.LENGTH_SHORT).show();
+
+                        LinearLayoutCompat pending_container = rootview.findViewById(R.id.pending_container);
+
+                        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+                        String[] row = response.split("<br>");
+
+                        for (String data : row) {
+
+                            View newview = inflater.inflate(R.layout.pending_table_row, pending_container, false);
+
+                            TextView date = newview.findViewById(R.id.date);
+                            TextView weight = newview.findViewById(R.id.weight);
+
+                            String[] columns = data.split(";");
+
+                            date.setText(columns[0]);
+                            weight.setText(columns[1]);
+
+
+
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "maliii", Toast.LENGTH_SHORT).show();
+                        Log.d("demo", error.toString());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", "Abrera.123");
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+
+
+
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pending, container, false);
 
+
+
+
+
+
     }
 
-    private void fetch_data(){
 
-        RequestQueue queue = Volley.newRequestQueue(context);
-        StringRequest sr = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                String[] row = response.split("<br>");
-
-                int i = 0;
-
-                while(i<row.length){
-                    String[] data = response.split(";");
-                    i++;
-
-                    String ton = data[0];
-                    String date = data[1];
-
-
-
-
-
-                }
-
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }){
-            @Override
-            protected Map<String,String> getParams(){
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("user","Abrera.123");
-
-                return params;
-            }
-
-        };
-        queue.add(sr);
-    }
 
 
 
