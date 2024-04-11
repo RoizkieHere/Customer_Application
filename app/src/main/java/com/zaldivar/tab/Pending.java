abd00849d8cbac6352d43d04f6988cc7262be2d9
RoizkieHere <roiabera14@gmail.com>
2024-04-11 11:17:56 +1000

    aretex (10/04/2024)

--- app/src/main/java/com/zaldivar/tab/Pending.java
+++ app/src/main/java/com/zaldivar/tab/Pending.java
@@ -6,13 +6,10 @@ import android.os.Bundle;
 import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
@@ -26,87 +23,79 @@ import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Pending extends Fragment {
 
+    Context context;
+    View rootview;
+    String url = "https://zaldivarservices.com/customer_app/pending.php";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View rootview = inflater.inflate(R.layout.fragment_pending, container, false);
-
-        String url = "https://zaldivarservices.com/customer_app/pending.php";
-        RequestQueue requestQueue = Volley.newRequestQueue(rootview.getContext());
-
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        LinearLayoutCompat container1 = (LinearLayoutCompat) rootview.findViewById(R.id.pending_container);
-
-                        LayoutInflater inflater = LayoutInflater.from(getContext());
-
-                        String[] row = response.split("<br>");
-
-                        for (String data : row) {
-
-                            View newview = inflater.inflate(R.layout.pending_table_row, container1, false);
-
-                            TextView date = newview.findViewById(R.id.date);
-                            TextView weight = newview.findViewById(R.id.weight);
-
-                            String[] columns = data.split(";");
-
-                            date.setText(columns[0]);
-                            weight.setText(columns[1]);
-
-
-
-                        }
-
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getContext(), "maliii", Toast.LENGTH_SHORT).show();
-                        Log.d("demo", error.toString());
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("username", "Abrera.123");
-                return params;
-            }
-        };
-
-        requestQueue.add(stringRequest);
-
-
-
-
-
-
+        context = getContext();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pending, container, false);
-
-
-
-
-
+        rootview =  inflater.inflate(R.layout.fragment_pending, container, false);
+        return  rootview;
 
     }
 
+    private void fetch_data(){
 
+        LinearLayoutCompat container = (LinearLayoutCompat)rootview.findViewById(R.id.pending_container) ;
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        StringRequest sr = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+                String[] row = response.split("<br>");
+
+                for (String column : row){
+
+                    String[] data = column.split(";");
+
+                    View new_view = inflater.inflate(R.layout.pending_table_row, container, false);
+
+                    TextView weight = new_view.findViewById(R.id.weight);
+                    TextView date = new_view.findViewById(R.id.date);
+                    TextView unit = new_view.findViewById(R.id.unit);
+
+                    weight.setText(data[0]);
+                    date.setText(data[1]);
+                    unit.setText("Ton");
+
+
+
+                }
+
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("user","Abrera.123");
+
+                return params;
+            }
+
+        };
+        queue.add(sr);
+    }
 
 
 
