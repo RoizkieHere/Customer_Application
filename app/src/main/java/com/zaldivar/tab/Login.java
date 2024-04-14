de20e56105bc46bd6551c8c47b297030657584a1
RoizkieHere <roiabrera14@gmail.com>
2024-04-14 12:39:33 +0800

    6:46 PM

--- app/src/main/java/com/zaldivar/tab/Login.java
+++ app/src/main/java/com/zaldivar/tab/Login.java
@@ -1,64 +1,147 @@
 package com.zaldivar.tab;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.core.widget.TextViewCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Login#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Login extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    View rootView;
+    TextView error_msg;
+    Context context;
+    Button login, sign_up;
+    EditText username, password;
+    String url = "Roi";
+    SharedPreferences sharedPreferences;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Login() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Login.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Login newInstance(String param1, String param2) {
-        Login fragment = new Login();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        rootView = inflater.inflate(R.layout.fragment_pending, container, false);
+        context = getContext();
+
+        error_msg.setVisibility(View.GONE);
+
+        login = rootView.findViewById(R.id.login_button);
+        sign_up = rootView.findViewById(R.id.sign_up);
+        username = rootView.findViewById(R.id.username);
+        password = rootView.findViewById(R.id.password);
+
+        error_msg = rootView.findViewById(R.id.error_message);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                validate_input();
+
+            }
+        });
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent sign_up = new Intent(context, Signup.class);
+                startActivity(sign_up);
+
+            }
+        });
+
+        return rootView;
     }
+
+    private void validate_input(){
+
+        String usernameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+                if (response.equals("Reject")){
+
+                    error_msg.setVisibility(View.VISIBLE);
+
+                } else if (response.equals("Not Existing")){
+
+                    error_msg.setVisibility(View.VISIBLE);
+                    error_msg.setText("Not existing. Please try again or Sign-up");
+
+                } else {
+                    error_msg.setVisibility(View.GONE);
+
+                    SharedPreferences.Editor editor= sharedPreferences.edit();
+                    editor.putString("username", usernameString);
+
+                    Intent login =  new Intent(context, MainActivity.class);
+                    startActivity(login);
+
+                }
+
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", usernameString);
+                params.put("password", passwordString);
+                return params;
+            }
+        };
+        queue.add(sr);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/zaldivar/tab/Signup.java
+++ app/src/main/java/com/zaldivar/tab/Signup.java
@@ -1,19 +1,105 @@
 package com.zaldivar.tab;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Signup extends Fragment {
+
+    Context context;
+    View rootView;
+
+    String url = "Roi";
+
+    EditText email_add;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_signup, container, false);
+        context = getContext();
+
+        AppCompatButton sign_up = rootView.findViewById(R.id.sign_up);
+        email_add = rootView.findViewById(R.id.email_address);
+
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                send_otp();
+            }
+        });
+
+        return rootView;
+
     }
+
+    private void send_otp(){
+
+        String emailString = email_add.getText().toString();
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", emailString);
+                return params;
+            }
+        };
+        queue.add(sr);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
