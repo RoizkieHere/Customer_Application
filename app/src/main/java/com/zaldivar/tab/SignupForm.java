5c7aa4227a3b4d4f14cc34d9c412e3c443c91aca
RoizkieHere <roiabera14@gmail.com>
2024-04-17 12:55:10 +1000

    aretex (10/04/2024)

--- app/src/main/java/com/zaldivar/tab/SignupForm.java
+++ app/src/main/java/com/zaldivar/tab/SignupForm.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,9 +18,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupForm extends AppCompatActivity {
+
+    public String usernameString,
+                passwordString,
+                confirmString,
+                firstname_string,
+                lastname_string,
+                phone_string,
+                address_string;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +73,6 @@ public class SignupForm extends AppCompatActivity {
         password_confirm = login_credentials.findViewById(R.id.password_confirm);
         AppCompatTextView sign_up_err = login_credentials.findViewById(R.id.sign_up_err);
 
-
-
         //Error message:
         AppCompatTextView pe_error_msg = personal_info.findViewById(R.id.error_msg);
 
@@ -65,10 +83,13 @@ public class SignupForm extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                String firstname_string = firstname.getText().toString();
-                String lastname_string = lastname.getText().toString();
-                String phone_string = phone_number.getText().toString();
-                String address_string = address.getText().toString();
+                firstname_string = firstname.getText().toString();
+                lastname_string = lastname.getText().toString();
+                phone_string = phone_number.getText().toString();
+                address_string = address.getText().toString();
+
+
+
 
                 if(firstname_string.isEmpty() || lastname_string.isEmpty() || phone_string.isEmpty() || address_string.isEmpty()){
 
@@ -77,20 +98,30 @@ public class SignupForm extends AppCompatActivity {
 
                     if(firstname_string.isEmpty()){
                         firstname.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    } else {
+                        firstname.setBackground(getResources().getDrawable(R.drawable.input_field, null));
                     }
 
                     if(lastname_string.isEmpty()){
                         lastname.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    } else {
+                        lastname.setBackground(getResources().getDrawable(R.drawable.input_field, null));
                     }
 
                     if(address_string.isEmpty()) {
                         address.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    } else {
+                        address.setBackground(getResources().getDrawable(R.drawable.input_field, null));
                     }
 
                     if(phone_string.isEmpty()) {
                         phone_number.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
-
+                    } else {
+                        phone_number.setBackground(getResources().getDrawable(R.drawable.input_field, null));
                     }
+
+
+
                 } else {
                     pe_error_msg.setVisibility(View.GONE);
                     firstname.setBackground(getResources().getDrawable(R.drawable.input_field, null));
@@ -105,39 +136,6 @@ public class SignupForm extends AppCompatActivity {
             }
         });
 
-        sign_up_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String usernameString = username.getText().toString();
-                String passwordString = password.getText().toString();
-                String confirmString = password_confirm.getText().toString();
-
-
-                if(usernameString.isEmpty() || passwordString.isEmpty() || confirmString.isEmpty()){
-
-                    sign_up_err.setVisibility(View.VISIBLE);
-                    sign_up_err.setText("Please fill out empty field/s.");
-
-                    if (usernameString.isEmpty()){
-                        username.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
-                    }if (passwordString.isEmpty()){
-                        password.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
-                    }if (confirmString.isEmpty()){
-                        password_confirm.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
-                    }
-
-                } else {
-                    sign_up_err.setVisibility(View.GONE);
-                    username.setBackground(getResources().getDrawable(R.drawable.input_field, null));
-                    password.setBackground(getResources().getDrawable(R.drawable.input_field, null));
-                    password_confirm.setBackground(getResources().getDrawable(R.drawable.input_field, null));
-                }
-
-
-            }
-        });
-
 
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -147,6 +145,100 @@ public class SignupForm extends AppCompatActivity {
             }
         });
 
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                usernameString = username.getText().toString();
+                passwordString = password.getText().toString();
+                confirmString = password_confirm.getText().toString();
+
+
+                if (usernameString.isEmpty() || passwordString.isEmpty() || confirmString.isEmpty()) {
+
+                    sign_up_err.setVisibility(View.VISIBLE);
+                    sign_up_err.setText("Please fill out empty field/s.");
+
+                    if (usernameString.isEmpty()) {
+                        username.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    } else {
+                        username.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+                    }
+
+                    if (passwordString.isEmpty()) {
+                        password.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    } else {
+                        password.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+                    }
+
+                    if (confirmString.isEmpty()) {
+                        password_confirm.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    } else {
+                        password_confirm.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+                    }
+
+                    if (!passwordString.equals(confirmString)){
+                        sign_up_err.setVisibility(View.VISIBLE);
+                        password.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                        password_confirm.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                        sign_up_err.setText("Passwords don't match!");
+
+                    } else {
+                        password.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+                        password_confirm.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+                    }
+
+                }  else {
+                        sign_up_err.setVisibility(View.GONE);
+                        username.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+                        password.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+                        password_confirm.setBackground(getResources().getDrawable(R.drawable.input_field, null));
+
+                        signup();
+
+
+                }
+
+            }
+        });
+
+    }
+
+    private void signup(){
+
+        String url = "https://zaldivarservices.com/android_new/customer_app/account/sign_up.php";
+
+        RequestQueue queue = Volley.newRequestQueue(SignupForm.this);
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                if(response.equals("Success")){
+                    Toast.makeText(SignupForm.this, "Data has been successfully stored into the database", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", usernameString);
+                params.put("firstname", firstname_string);
+                params.put("lastname", lastname_string);
+                params.put("phone_number", phone_string);
+                params.put("address", address_string);
+                params.put("password", passwordString);
+                return params;
+            }
+        };
+        queue.add(sr);
+
+
 
 
 
@@ -154,4 +246,10 @@ public class SignupForm extends AppCompatActivity {
 
 
     }
+
+
+
+
+
+
 }
