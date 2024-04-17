a22967263a55c7ce7ff56dc129244962a0c076a4
RoizkieHere <roiabera14@gmail.com>
2024-04-17 10:35:30 +1000

    aretex (10/04/2024)

--- app/src/main/java/com/zaldivar/tab/SignupForm.java
+++ app/src/main/java/com/zaldivar/tab/SignupForm.java
@@ -34,16 +34,34 @@ public class SignupForm extends AppCompatActivity {
         View login_credentials = LayoutInflater.from(SignupForm.this).inflate(R.layout.login_credentials, container, false);
 
 
-        AppCompatButton next_button = personal_info.findViewById(R.id.next_button);
-        AppCompatButton sign_in_button = login_credentials.findViewById(R.id.sign_in_button);
-
-
         //For personal info:
+        AppCompatButton next_button = personal_info.findViewById(R.id.next_button);
         EditText firstname, lastname, address, phone_number;
         firstname = personal_info.findViewById(R.id.firstname);
         lastname = personal_info.findViewById(R.id.lastname);
         address = personal_info.findViewById(R.id.address);
         phone_number = personal_info.findViewById(R.id.phone_num);
+
+        String firstname_string = firstname.getText().toString();
+        String lastname_string = lastname.getText().toString();
+        String phone_string = phone_number.getText().toString();
+        String address_string = address.getText().toString();
+
+        //For login_credentials:
+        AppCompatButton sign_up_button = login_credentials.findViewById(R.id.sign_up_button);
+        EditText username, password, password_confirm;
+        username = login_credentials.findViewById(R.id.username);
+        password = login_credentials.findViewById(R.id.password);
+        password_confirm = login_credentials.findViewById(R.id.password_confirm);
+        TextView sign_up_err = login_credentials.findViewById(R.id.sign_up_err);
+
+        String usernameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+        String confirmString = password_confirm.getText().toString();
+
+
+        //Error message:
+        TextView pe_error_msg = personal_info.findViewById(R.id.error_msg);
 
         container.addView(personal_info);
 
@@ -52,29 +70,61 @@ public class SignupForm extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                if(firstname.getText().toString().isEmpty()){
-                    firstname.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+
+                if(firstname_string.isEmpty() || lastname_string.isEmpty() || phone_string.isEmpty() || address_string.isEmpty()){
+
+                    pe_error_msg.setVisibility(View.VISIBLE);
+                    pe_error_msg.setText("Please fill-out empty field/s.");
+
+                    if(firstname_string.isEmpty()){
+                        firstname.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    }
+
+                    if(lastname_string.isEmpty()){
+                        lastname.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    }
+
+                    if(address_string.isEmpty()) {
+                        address.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    }
+
+                    if(phone_string.isEmpty()) {
+                        phone_number.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+
+                    }
+                } else {
+                    pe_error_msg.setVisibility(View.GONE);
+                    container.addView(login_credentials);
                 }
-
-                if(lastname.getText().toString().isEmpty()){
-                    lastname.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
-                }
-
-                if(address.getText().toString().isEmpty()) {
-                    address.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
-                }
-
-                if(phone_number.getText().toString().isEmpty()) {
-                    phone_number.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
-
-                }
-
             }
         });
 
-        sign_in_button.setOnClickListener(new View.OnClickListener() {
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                if(usernameString.isEmpty() || passwordString.isEmpty() || confirmString.isEmpty()){
+
+                    sign_up_err.setVisibility(View.VISIBLE);
+                    sign_up_err.setText("Please fill out empty field/s.");
+
+                    if (usernameString.isEmpty()){
+                        username.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    }if (passwordString.isEmpty()){
+                        password.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    }if (confirmString.isEmpty()){
+                        password_confirm.setBackground(getResources().getDrawable(R.drawable.error_input_field, null));
+                    }
+
+                    
+                }
+
+
+
+
+
+
 
             }
         });
