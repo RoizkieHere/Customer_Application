3109f0d9f276ee8e4ae23ff31f7b194d2c5c69b9
RoizkieHere <roiabrera14@gmail.com>
2024-04-25 16:18:15 +0800

    6:46 PM

--- app/src/main/java/com/zaldivar/tab/Profile.java
+++ app/src/main/java/com/zaldivar/tab/Profile.java
@@ -1,6 +1,10 @@
 package com.zaldivar.tab;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,27 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        EditText username = findViewById(R.id.profile_username);
+        EditText contact_num = findViewById(R.id.profile_contact);
+        TextView delete_account = findViewById(R.id.delete_acc);
+        Button update = findViewById(R.id.update_button);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update_info();
+            }
+        });
+
+    }
+
+    private void update_info(){
+
+
+
+
+
+
     }
 }
